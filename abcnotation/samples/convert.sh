2017ecc9f582c6e54e4ddd36aa3762e72bb039bf
farvardin <farvardin>
2020-05-23 15:55:18 +0000

    adding abc binaries adding abc sample

---
+++ abcnotation/samples/convert.sh
@@ -0,0 +1,6 @@
+../bin/abcm2ps -X $1
+../bin/abc2midi $1
+
+open Out.xhtml &
+
+
--- abcnotation/src/script.sh
+++ abcnotation/src/script.sh
@@ -4,3 +4,13 @@ git clone https://github.com/leesavide/abcm2ps/
 
 wget https://ifdo.ca/~seymour/runabc/abcMIDI-2020.05.06.zip
 
+ unzip abcMIDI-*
+
+cd abcmidi
+./configure
+make -j4
+
+cd ..
+cd abcm2ps
+echo "You'll have to fix the code at the errors, just remove the ifdef and move the ; at the end of the line"
+
